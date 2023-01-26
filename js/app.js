62eea09b19f3270878a1f8fe1f9a85680645833b
Dylan Blackwell <tophatrev@gmail.com>
2023-01-26 13:58:39 +0000

    Sum and multiply.

--- js/app.js
+++ js/app.js
@@ -9,11 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+  let mySum = a + b;
+  let myString = "The sum of " + a + " and " + b + " is " + mySum + "."
 
+  return[mySum, myString];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  let myMultiply = a * b;
+  let myString = "The product of " + a + " and " + b + " is " + myMultiply + "."
 
+  return[myMultiply, myString]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
